10bb40ccc2d4ee4b5c0a3578cc05c34da15ee385
Saba Motto <sabapotcom@yahoo.co.jp>
2019-06-25 15:37:24 +0900

    Improve modal view

--- assets/js/modals/ConfirmationModal.vue
+++ assets/js/modals/ConfirmationModal.vue
@@ -1,6 +1,6 @@
 <template>
-  <transition name="modal">
-    <div class="modal-mask">
+  <transition name="modal" appear>
+    <div class="modal-mask" @click.self="$emit('cancel')">
       <div class="modal-wrapper">
         <div class="modal-container">
 
@@ -16,12 +16,8 @@
 
           <div class="modal-footer">
             <slot name="footer">
-              <button class="modal-default-button" @click="$emit('ok')">
-                OK
-              </button>
-              <button class="modal-default-button" @click="$emit('cancel')">
-                Cancel
-              </button>
+              <button class="modal-default-button" @click="$emit('ok')">{{ ok }}</button>
+              <button class="modal-default-button" @click="$emit('cancel')">{{ cancel }}</button>
             </slot>
           </div>
         </div>
@@ -32,50 +28,47 @@
 
 <script>
 export default {
+  props: {
+    ok: {default: 'OK'},
+    cancel: {default: 'Cancel'},
+  }
 }
 </script>
 
-<style scoped>
-.modal-mask {
-  position: fixed;
-  z-index: 19998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .5);
-  display: table;
-  transition: opacity .3s ease;
-}
+<style scoped lang="stylus">
+.modal-mask
+  display: flex
+  align-items: center
+  justify-content: center
+  position: fixed
+  z-index: 19998
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  background-color: rgba(0, 0, 0, .5)
+  transition: opacity .3s ease
 
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
+.modal-container
+  width: 300px
+  margin: 0px auto
+  padding: 20px 30px
+  background-color: #fff
+  border-radius: 2px
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33)
+  transition: all .3s ease
+  font-family: Helvetica, Arial, sans-serif
 
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
+.modal-header h3
+  margin-top: 0
+  color: #42b983
 
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
+.modal-body
+  margin: 20px 0
 
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
+.modal-default-button
+  float: right
+  margin-right: 4px
 
 /*
  * The following styles are auto-applied to elements with
@@ -86,17 +79,14 @@ export default {
  * these styles.
  */
 
-.modal-enter {
-  opacity: 0;
-}
+.modal-enter
+  opacity: 0
 
-.modal-leave-active {
-  opacity: 0;
-}
+.modal-leave-active
+  opacity: 0
 
 .modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
+.modal-leave-active .modal-container
+  -webkit-transform: scale(1.1)
+  transform: scale(1.1)
 </style>
